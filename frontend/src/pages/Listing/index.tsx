fc168fcd831c6903d28189c2554b0d6a724c97d0
gehhanna <ge.hanna2015@gmail.com>
2022-05-12 13:25:19 -0300

    Security Config

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,13 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
 
     return (
+        <>
         <Pagination/>
+        <MovieCard/>
+        </>
     );
 }
 
